c63de3810dcf65b77210308e5daf279a6dd6e045
Mik-C <miquelclark@hotmail.com>
2021-02-10 20:18:14 +0100

    Testing node.js integration

--- node-app/main.js
+++ node-app/main.js
@@ -1,4 +1,21 @@
+/*
 //TODO
-//litegraph dependency:
-//  var LiteGraph = require("./litegraph.js").LiteGraph;
-//  Note that not all nodes will work
+//main.js should act as a websocket server instead of as a client so third applications can connect directly
+
+var UTILS = require("../src/utils.js").UTILS;
+var Streamer = require("../src/streamer.js").Streamer;
+var streamer = new Streamer();
+console.log(streamer);
+*/
+
+//Dependencies:
+//npm install gl-matrix
+
+//Base of node graph system. (There are some modifications to make it work properly with gl-matrix)
+var LiteGraph = require("../src/libs/litegraph").LiteGraph;
+
+//Expand graph system with behaviour trees and many nodes
+var HBTree = require("../src/libs/HBTree")(LiteGraph);
+var graphNodes = require("../src/graphNodes")(LiteGraph, HBTree);
+
+//console.log(LiteGraph.Nodes);
