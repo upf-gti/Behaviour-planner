536521b6ffce85e6911a3dcf54ed07cd5fd5c62c
evalls <evallsg4@gmail.com>
2021-03-18 15:20:32 +0100

    send parsed JSON to onDataReceived function

--- web-app/js/streamer.js
+++ web-app/js/streamer.js
@@ -73,7 +73,7 @@ Streamer.prototype.processMessage = function(data){
 	if( data.constructor === ArrayBuffer ){
 		if(this.onArrayDataReceived) this.onArrayDataReceived(data);
 	}else if(data.constructor === String ){
-		if(this.onDataReceived) this.onDataReceived(data);
+		if(this.onDataReceived) this.onDataReceived(JSON.parse(data));
 	}else{
 		console.warn("Unknown message type");
 	}
