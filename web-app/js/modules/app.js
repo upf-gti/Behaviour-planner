baf29ca2a45a44604de245ecc29d2eb1be040db1
evalls <evallsg4@gmail.com>
2021-10-05 12:39:47 +0200

    canvas keyup modified

--- web-app/js/modules/app.js
+++ web-app/js/modules/app.js
@@ -46,7 +46,8 @@ class App{
 
 	    this.streamer = new Streamer("wss://webglstudio.org/port/9003/ws/");
 	    this.streamer.onDataReceived = this.onDataReceived.bind(this);
-			this.streamer.onConnect = this.onWSconnected.bind(this);
+			
+        this.streamer.onConnect = this.onWSconnected.bind(this);
 
 	    this.chat = new Chat();
 		this.iframe =   CORE.Interface.iframe;
